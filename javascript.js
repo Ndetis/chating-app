f7991f17f79d938ee5de4f1a49457ac0b5d4a644
Ndetis Elie <nyobedjobelie@gmail.com>
2021-10-08 19:12:28 +0200

    avater_modify_unfinished

---
+++ javascript.js
@@ -0,0 +1,11 @@
+let avater = document.querySelectorAll('.friend--onhover .user-photo img src');
+console.log(avater);
+let list = avater.src;
+console.log(list);
+/* let profil = avater.querySelector('.user-photo');
+console.log(profil);
+let friend = document.querySelectorAll('.friend user-photo');
+ */
+/* avater.addEventListener('click',()=> {debugger
+    friend.img.src = "profil.img.src"; 
+}) */
